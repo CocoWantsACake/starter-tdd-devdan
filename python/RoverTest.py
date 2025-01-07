7d266c2fbc88e85406fc9819685551307e70a3e8
CocoWantsACake <maxime27130@outlook.fr>
2025-01-07 17:00:48 +0100

    hehe2

--- python/RoverTest.py
+++ python/RoverTest.py
@@ -1,43 +1,36 @@
 import pytest
 from prod_code import Direction
 from prod_code import Rover
+from prod_code import MarsMap
 
-# marsMap = MarsMap(100, 100)
+marsMap = MarsMap(100, 100)
 
 def test_faces_north_on_landing():
-    rover = Rover()
+    rover = Rover(marsMap)
     assert rover.direction == Direction.NORTH
 
-def test_rover_moves_forward_ten_times():
-    rover = Rover(50, 50)
+def simple_forward_movement_of_the_rover():
+    marsMap2 = MarsMap(100, 100)
+    for i in range(10):
+        marsMap2.marsMap[0][i].cellType = None
+
+    rover = Rover(marsMap2)
     for i in range(10):
         rover.moveForward()
 
-    assert rover.xPos == 50
-    assert rover.yPos == 40
+    assert rover.xPos == 0
+    assert rover.yPos == 10
 
-def test_rover_moves_forward_east_direction_fifteen_times():
-    rover = Rover(50, 50)
-    for i in range(10):
-        rover.moveForward()
+    assert rover.obstacle_stopped is False
+    assert rover.history == [Direction.NORTH for i in range(10)]
 
-    assert rover.xPos == 50
-    assert rover.yPos == 40
-
-
-
-
+def rover_rotation():
+    rover = Rover(marsMap)
+    rover.turnLeft()
+    assert rover.direction == Direction.WEST
+    rover.turnRight()
+    assert rover.direction == Direction.NORTH
 
 
 
-"""
-def moving_forward_10_times():
-    rover = Rover(50, 50)
-    rover.forward()
 
-    xPos = rover.currCell.xPos
-    yPos = rover.currCell.yPos
-
-    assert xPos == 50
-    assert yPos == 40
-"""
--- python/prod_code.py
+++ python/prod_code.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from random import randint
 
 class Direction(Enum):
     NORTH = 1
@@ -6,26 +7,72 @@ class Direction(Enum):
     EAST = 3
     WEST = 4
 
+
+class Cell:
+    cellType = None
+    xPos = None
+    yPos = None
+
+    def __init__(self, xPos, yPos, cellType="flat"):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.cellType = cellType
+
+    def isObstacle(self):
+        return self.cellType in ["obstacle"]
+
+
+class MarsMap:
+    xLength = None
+    yLength = None
+    marsMap = None
+
+    def __init__(self, xLength, yLength):
+        self.xLength = xLength
+        self.yLength = yLength
+        self.marsMap = [[Cell(i, j) for j in range(yLength)] for i in range(xLength)]
+
+        for i in range(5):
+            xPosObst = randint(0, xLength-1)
+            yPosObst = randint(0, yLength-1)
+            self.marsMap[xPosObst][yPosObst].cellType = "obstacle"
+
+
+
+
+
 class Rover:
     xPos = None
     yPos = None
     direction = None
+    history = []
+    obstacle_stopped = False
+    marsMap = None
 
-    def __init__(self, x=0, y=0):
+    def __init__(self, marsMap, x=0, y=0):
+        self.marsMap = marsMap
         self.xPos = x
         self.yPos = y
         self.direction = Direction.NORTH
 
     def moveForward(self):
         if self.direction is Direction.NORTH:
-            self.yPos -= 1
+            self.yPos += 1
 
         if self.direction is Direction.SOUTH:
-            self.yPos += 1
+            self.yPos -= 1
 
         if self.direction is Direction.EAST:
             self.xPos += 1
 
         if self.direction is Direction.WEST:
             self.xPos -= 1
+
+        self.history.append(self.direction)
+
+    def turnLeft(self):
+        pass
+
+    def turnRight(self):
+        pass
 
